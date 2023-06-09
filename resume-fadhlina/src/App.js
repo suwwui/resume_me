9754fe12a61127e1f2443eea0dc8b7a3695a4bf6
fdhlyna <128072211+suwwui@users.noreply.github.com>
2023-06-10 03:56:01 +0800

    first

---
+++ resume-fadhlina/src/App.js
@@ -0,0 +1,164 @@
+import './App.css';
+import React from 'react';
+
+const Resume = () => {
+  return (
+    <div>
+      <header>
+        <h1 style={{ color: '#137557' }}>Nur Fadhlina Wjedi</h1>
+        <p>014-7740908 | Johor Bahru |wfdhlyna@gmail.com| Nur Fadhlina | suwwui</p>
+      </header>
+      <section>
+        <h2 style={{ color: '#137557' }}>EDUCATION</h2>
+        <ul>
+          <li>
+            <h3>Universiti Malaysia Sarawak, UNIMAS (2019-2022)</h3>
+            <h4>Bachelor in Cognitive Science with Honors</h4>
+            <ul>
+            <li>3.64/4.0</li>
+            <li>MUET: Band 4</li>
+            <li>Sekretariat Sukan dan Rekreasi, Faculty of Cognitive Science and Human Development (2019-2020)</li>
+            </ul>
+          </li>
+          <li>
+            <h3>Pusat Pengajian Pra-Universiti, UNIMAS (2018-2019)</h3>
+            <h4>Foundation in Life Science</h4>
+          </li>
+        </ul>
+      </section>
+      <section>
+        <h2 style={{ color: '#137557' }}>WORKING EXPERIENCE</h2>
+        <ul>
+          <li>
+            <h3>Sales Assistant (Part Time)</h3>
+            <p>QBerries Bakery, Johor Bahru (Feb-Oct,2021)</p>
+            <ul>
+              <li>Maintained the store environment and replenished the store inventory supply with FIFO concept</li>
+              <li>Key selling products inside the POS system and filled documents related to payment receipt and invoice into labelled file</li>
+              <li>Quality checking for the store goods production and report back to the management</li>
+            </ul>
+          </li>
+          <li>
+            <h3>UI/UX Designer Intern</h3>
+            <p>Imagine AI, Johor Bahru (July-Oct,2021)</p>
+            <ul>
+              <li>Did market research on a software for pitching preparation for MRCB-ICP grant regarding the palm oil project.</li>
+              <li>Developed and conceptualized comprehensive UI/UX design strategy for the ongoing software projects of the company using Adobe XD and Illustrator</li>
+              <li>Collaborate with the marketing team and technical team to ensure the creation and delivery of tailored experiences for the digital user.</li>
+            </ul>
+          </li>
+          <li>
+            <h3>Software Developer Trainee</h3>
+            <p>Imagine AI, Johor Bahru (July-Oct,2021)</p>
+            <ul>
+              <li>Worked with other developers to design algorithms, interface and flowcharts for the proposed system based on client needs.</li>
+              <li>Gathered and evaluated user feedback by keeping in-touch with clients for updates during the system development and implementation.</li>
+              <li>Created technical documentation such hardware list, market research and quotation for reference and reporting.</li>
+              <li>Involved with the projects that utilized the React JS framework and standardized all proposed software interface by building GUI using Qt Designer</li>
+            </ul>
+          </li>
+          <li>
+            <h3>Operation Manager</h3>
+            <p>QBerries Bakery, Johor Bahru (Oct 2022-Feb 2023)</p>
+            <ul>
+              <li>Build the formula and standardized the sales monthly report with 2 store branches using MS Excel</li>
+              <li>Developed, implemented, and reviewed operational policies and procedures such product SOP, Q&A customer booklet and staff training logbook</li>
+              <li>Oversees any issues and address the problem with the directors for problem solving approach</li>
+            </ul>
+          </li>
+        </ul>
+      </section>
+      <section>
+        <h2 style={{ color: '#137557' }}>ACTIVITIES AND INVOLVEMENT</h2>
+        <ul>
+          <li>
+            <h3>Softskills and Volunteerism, PPPU UNIMAS (2018)</h3>
+            <ul>
+              <li>Supervised a community work in Kampung Muara Tuang, Kota Samarahan, Sarawak and assisted in postmortem for the cashflow tracking and evaluation process</li>
+            </ul>
+          </li>
+          <li>
+            <h3>4th place in Asasi Biology Colloquium, PPPU UNIMAS (2018)</h3>
+            <ul>
+              <li>Collaborate in researching and analyzing of Pteridophytes plant species habitat at the Gunung Santubong Recreational Park</li>
+            </ul>
+          </li>
+          <li>
+            <h3>Citrawarna, UNIMAS (2019)</h3>
+            <ul>
+              <li>Competed with 14 teams in dancing competition for Kolej Allamanda the start of Unimas’ 23rd Convocation Festival (Confest Unimas)</li>
+            </ul>
+          </li>
+          <li>
+            <h3>1st place in Linguistic PlayDay, Faculty of Cognitive Science and Human Development (2019)</h3>
+            <ul>
+              <li>Workshop and competition to encourage student in designed a phonetics learning tools for assisting the introduction in production of speech sound by using Jenga blocks</li>
+            </ul>
+          </li>
+          <li>
+            <h3>1st place in Climate Change-Maker Challenge Hackathon, CreativeCulture UNIMAS (2022)</h3>
+            <ul>
+              <li>Workshop and competition to educate the Kampung Pasir Pandak, Kuching community on the climate change and designed a pest detector by using the Arduino kit to replace the chemical repeller used during the food processing in the community.</li>
+            </ul>
+          </li>
+        </ul>
+      </section>
+      <section>
+        <h2 style={{ color: '#137557' }}>PROJECT EXPERIENCE</h2>
+        <ul>
+          <li>
+            <h3>2018</h3>
+            <ul>
+              <li>Lead a group project to design and build a Coffee Vending Machine System for Basic Programming by using C++ language in Dev C++</li>
+            </ul>
+          </li>
+          <li>
+            <h3>2020</h3>
+            <ul>
+              <li>Lead a group project to prepare the feasibility and mock-up for Supervisee-supervisor System with Adobe XD in Expert System</li>
+            </ul>
+          </li>
+          <li>
+            <h3>2021</h3>
+            <ul>
+              <li>Lead group project to design and develop a functional circle detection software and the user interface with Octave in Computer Vision subject</li>
+            </ul>
+          </li>
+          <li>
+            <h3>2022</h3>
+            <ul>
+              <li>- Lead group project to prepare the user specification and mock-up for Aturee System, a productivity application for student with Figma in Human Computer Interaction</li>
+              <li>Design a functional mobile application for TimeTable and Chatroom with Android Studio (Kotlin) and Firebase in Mobile Computing</li>
+              <li>Prepare and develop a functional web-based system for SME’s sales and stock management for Final Year Project called i-Log Inventory System</li>
+            </ul>
+          </li>
+          </ul>
+          </section>
+      <section>
+        <h3 style={{ color: '#137557' }}>SKILLS</h3>
+        <ul>
+          <li>
+        <ul>
+            <li>
+              <h3>Programming Language:</h3>HTML, PHP, JavaScript, MySQL, Firebase, Kotlin, React JS, Tailwind, Octave, C++
+              </li>
+        </ul>
+        <ul>
+            <li>
+              <h3>Software:</h3> Microsoft Word/Powerpoint/Excel/Access, Android Studio, GitHub, Arduino, Hostinger, WordPress, Figma, Adobe XD, Canva, Adobe Illustrator, RapidMiner
+            </li>
+        </ul>
+        <ul>
+            <li>
+              <h3>Language:</h3> English and Malay (Fluent)
+            </li>
+        </ul>
+        </li>
+        </ul>
+      </section>
+    </div>
+  );
+};
+
+export default Resume;
+
